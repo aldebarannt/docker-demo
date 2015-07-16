3f75c7fe956561848ef49db15f787f5a695668f4
aldebaran <aldebaran@poczta.fm>
2015-07-16 21:01:31 +0200

    docker demo

--- node/nodeapp/server.js
+++ node/nodeapp/server.js
@@ -12,7 +12,7 @@ app.configure(function() {
   app.use(express.cookieParser('keyboard-cat'));
   app.use(express.session({
         store: new RedisStore({
-            host: process.env.REDIS_HOST || 'locahost',
+            host: process.env.REDIS_HOST || '192.168.99.100',
             port: process.env.REDIS_PORT || 6379,
             db: process.env.REDIS_DB || 0
         }),
